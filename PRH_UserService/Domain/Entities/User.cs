c90608ad3e30b67f9b5e0421edb35543dca0ab45
dattgt <trangiathanhdat02092002@gmail.com>
2024-09-28 22:01:32 +0700

    first commit in this branch

---
+++ PRH_UserService/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; init; }
+        public string FullName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int status { get; set; } = 0;
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public int RoleId { get; set; }
+    }
+}
---
+++ PRH_UserService/Infrastructure/Context/HFDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.Context
+{
+    public partial class HFDbContext : DbContext
+    {
+        public HFDbContext() { }
+
+        public HFDbContext(DbContextOptions<HFDbContext> options) : base(options) { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //     => optionsBuilder.UseNpgsql("Host=localhost; Database=CleanDb; Username=postgres; Password=Abcd1234");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+            modelBuilder.Entity<Role>().HasKey(r => r.Id);
+            modelBuilder.Entity<Role>().HasIndex(r => r.Name).IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasOne<Role>()
+                .WithMany(r => r.Users)
+                .HasForeignKey(p => p.RoleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
