02e30af333a01155a96d11f919d54ddd7c08e9f4
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-10-05 14:39:12 +0700

    update notification service

--- PRH_NotificationService/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ PRH_NotificationService/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee91740ccd440b22fd20cf47165d5613e958d526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a13259017fe111fcb472ab819007304d7d3342")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
