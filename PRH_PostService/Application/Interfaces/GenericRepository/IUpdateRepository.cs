5c173757c9b24d7dfc0e4024de185849beeaf400
dattgt <trangiathanhdat02092002@gmail.com>
2024-10-05 14:07:10 +0700

    push Post Service

---
+++ PRH_PostService/Application/Interfaces/GenericRepository/IUpdateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.GenericRepository
+{
+    public interface IUpdateRepository<in T> where T : class
+    {
+        Task Update(Guid id, T entity);
+    }
+}
