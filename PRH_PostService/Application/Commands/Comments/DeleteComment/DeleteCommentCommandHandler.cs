16d3772d072fa3f0bd6886e58da2e3e1cfd9afd4
dattgt <trangiathanhdat02092002@gmail.com>
2024-10-16 18:06:32 +0700

    feat: implement comment

---
+++ PRH_PostService/Application/Commands/Comments/DeleteComment/DeleteCommentCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Commons;
+using Application.Interfaces.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Comments.DeleteComment
+{
+    public class DeleteCommentCommandHandler(ICommentRepository commentRepository)
+        : IRequestHandler<DeleteCommentCommand, BaseResponse<string>>
+    {
+        public async Task<BaseResponse<string>> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<string>
+            {
+                Id = request.Id,
+                Timestamp = DateTime.UtcNow,
+                Errors = new List<string>()
+            };
+            try
+            {
+                await commentRepository.DeleteAsync(request.Id);
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Success = true;
+                response.Message = "Comment deleted successfully";
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Success = false;
+                response.Message = "Failed to delete comment";
+                response.Errors.Add(ex.Message);
+            }
+            return response;
+        }
+    }
+}
---
+++ PRH_PostService/Application/Commands/Comments/UpdateComment/UpdateCommentCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons;
+using Application.Commons.DTOs;
+using MediatR;
+
+namespace Application.Commands.Comments.UpdateComment
+{
+    public record UpdateCommentCommand(Guid commentId, CommentDto CommentDto) : IRequest<BaseResponse<string>>;
+}
