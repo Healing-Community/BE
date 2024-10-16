00c112fdfee58c41fa43e38fd6f25bd36800b0a5
dattgt <trangiathanhdat02092002@gmail.com>
2024-10-16 18:05:36 +0700

    feat: implement reaction type

---
+++ PRH_PostService/Application/Commands/ReactionTypes/AddReactionType/CreateReactionTypeCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Application.Commons;
+using Application.Interfaces.Repository;
+using Domain.Entities;
+using MassTransit;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.ReactionTypes.AddReactionType
+{
+    public class CreateReactionTypeCommandHandler(IReactionTypeRepository reactionTypeRepository) 
+        : IRequestHandler<CreateReactionTypeCommand, BaseResponse<string>>
+    {
+        public async Task<BaseResponse<string>> Handle(CreateReactionTypeCommand request, CancellationToken cancellationToken)
+        {
+            var reactionTypeId = NewId.NextSequentialGuid();
+            var reactionType = new ReactionType
+            {
+                ReactionTypeId = reactionTypeId,
+                Name = request.ReactionTypeDto.Name,
+            };
+            var response = new BaseResponse<string>
+            {
+                Id = reactionTypeId,
+                Timestamp = DateTime.UtcNow,
+                Errors = new List<string>()
+            };
+            try
+            {
+                await reactionTypeRepository.Create(reactionType);
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Success = true;
+                response.Message = "Reaction type created successfully";
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Success = false;
+                response.Message = "Failed to create reaction type";
+                response.Errors.Add(ex.Message);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ PRH_PostService/Application/Commands/ReactionTypes/UpdateReactionType/UpdateReactionTypeCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Application.Commons;
+using Application.Interfaces.Repository;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.ReactionTypes.UpdateReactionType
+{
+    public class UpdateReactionTypeCommandHandler(IReactionTypeRepository reactionTypeRepository)
+        : IRequestHandler<UpdateReactionTypeCommand, BaseResponse<string>>
+    {
+        public async Task<BaseResponse<string>> Handle(UpdateReactionTypeCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<string>
+            {
+                Id = request.reactionTypeId,
+                Timestamp = DateTime.UtcNow,
+                Errors = new List<string>()
+            };
+            try
+            {
+                var existingReaction = await reactionTypeRepository.GetByIdAsync(request.reactionTypeId);
+                var updatedReaction = new ReactionType
+                {
+                    ReactionTypeId = request.reactionTypeId,
+                    Name = request.ReactionTypeDto.Name,
+                };
+                await reactionTypeRepository.Update(request.reactionTypeId, updatedReaction);
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Success = true;
+                response.Message = "Reaction type updated successfully";
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Success = false;
+                response.Message = "Failed to update reaction type";
+                response.Errors.Add(ex.Message);
+            }
+            return response;
+        }
+    }
+}
