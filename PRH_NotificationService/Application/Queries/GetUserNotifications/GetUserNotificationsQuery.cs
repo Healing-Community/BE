619553e7312b0c4ef93c1c310cacce8c78941180
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-10-17 18:36:20 +0700

    feat: notifications

---
+++ PRH_NotificationService/Application/Queries/GetUserNotifications/GetUserNotificationsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons;
+using Application.Commons.DTOs;
+using MediatR;
+
+namespace Application.Queries.GetUserNotifications
+{
+    public record GetUserNotificationsQuery(Guid UserId, bool IncludeRead) : IRequest<BaseResponse<List<NotificationDto>>>;
+}
