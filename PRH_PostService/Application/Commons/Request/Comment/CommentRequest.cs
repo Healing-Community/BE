d68e21081a89ff46fe422ddf4ccd7646fbc4ded2
dattgt <trangiathanhdat02092002@gmail.com>
2024-10-18 21:01:40 +0700

    modified path for comment, post, reaction and report

--- PRH_PostService/Application/Commons/Request/Comment/CommentRequest.cs
+++ PRH_PostService/Application/Commons/Request/Comment/CommentRequest.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Commons.Request
+namespace Application.Commons.Request.Comment
 {
-    public class CommentRequestCreatedMessage
+    public class CommentRequest
     {
-        public Guid CommentRequestId { get; set; }
+        public Guid CommentId { get; set; }
         public Guid PostId { get; set; }
-        //public Guid? ParentId { get; set; }
+        public Guid? ParentId { get; set; }
         public Guid UserId { get; set; }
         public string? Content { get; set; }
-        public DateTime CommentedDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
--- PRH_PostService/Application/Commons/Request/Comment/CommentRequestCreatedMessage.cs
+++ PRH_PostService/Application/Commons/Request/Comment/CommentRequestCreatedMessage.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Commons.Request
+namespace Application.Commons.Request.Comment
 {
-    public class CommentRequest
+    public class CommentRequestCreatedMessage
     {
-        public Guid CommentId { get; set; }
+        public Guid CommentRequestId { get; set; }
         public Guid PostId { get; set; }
-        public Guid? ParentId { get; set; }
+        //public Guid? ParentId { get; set; }
         public Guid UserId { get; set; }
         public string? Content { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime CommentedDate { get; set; }
     }
 }
