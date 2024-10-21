4c4e40199c9ed826dd523f111740a240e702a832
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-10-21 13:59:57 +0700

    fix: noti

--- PRH_NotificationService/Application/Commons/Request/Report/ReportRequestCreatedMessage.cs
+++ PRH_NotificationService/Application/Commons/Request/Report/ReportRequestCreatedMessage.cs
@@ -2,10 +2,10 @@
 {
     public class ReportRequestCreatedMessage
     {
-        public Guid ReportRequestId { get; set; }
-        public Guid PostId { get; set; }
-        public Guid UserId { get; set; }
-        public Guid ReportTypeId { get; set; }
+        public required string ReportRequestId { get; set; }
+        public string? PostId { get; set; }
+        public string? UserId { get; set; }
+        public string? ReportTypeId { get; set; }
         public int Status { get; set; }
         public DateTime ReportedDate { get; set; }
     }
