f241a8670dc54cf394161ea4c81b36c052057621
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-11-03 14:14:00 +0700

    create project and config ci/cd for Payment Service

---
+++ PRH_PaymentService/Application/Commons/Tools/Authentication.cs
@@ -0,0 +1,39 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Commons.Tools;
+
+public static class Authentication
+{
+    public static string? GetUserIdFromHttpContext(HttpContext httpContext)
+    {
+        // Check if the Authorization header exists
+        if (!httpContext.Request.Headers.TryGetValue("Authorization", out Microsoft.Extensions.Primitives.StringValues value))
+            return null;
+
+        string? authorizationHeader = value;
+
+        // Validate the Authorization header format
+        if (string.IsNullOrWhiteSpace(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
+            return null;
+
+        var jwtToken = authorizationHeader["Bearer ".Length..];
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            // Read the JWT token
+            var token = tokenHandler.ReadJwtToken(jwtToken);
+            var idClaim = token.Claims.FirstOrDefault(claim =>
+                claim.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+
+            // Return the user ID if found, otherwise return null
+            return idClaim?.Value;
+        }
+        catch (Exception)
+        {
+            // Return null if there's any issue parsing the token or getting the claim
+            return null;
+        }
+    }
+}
---
+++ PRH_PaymentService/PRH_PaymentService_API/Middleware/AuthMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PRH_ExpertService_API.Middleware
+{
+    public class AuthMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            var endpoint = context.GetEndpoint();
+            var allowAnonymous = endpoint?.Metadata.GetMetadata<AllowAnonymousAttribute>();
+            if (allowAnonymous != null)
+            {
+                await next(context);
+                return;
+            }
+
+            if (context.Request.Headers.TryGetValue("Authorization", out Microsoft.Extensions.Primitives.StringValues value))
+            {
+                var token = value.ToString().Replace("Bearer ", "");
+                if (!string.IsNullOrEmpty(token))
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    try
+                    {
+                        var jwtSettings = configuration.GetSection("JwtSettings");
+                        var validationParameters = new TokenValidationParameters
+                        {
+                            ValidIssuer = jwtSettings["Issuer"],
+                            ValidAudience = jwtSettings["Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey
+                                (Encoding.UTF8.GetBytes(jwtSettings["Secret"] ?? throw new InvalidOperationException())),
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidateLifetime = false // Disable automatic lifetime validation
+                        };
+
+                        // Validate the token without lifetime check
+                        var principal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+                        var jwtToken = validatedToken as JwtSecurityToken;
+
+                        // Extract expiration time and compare with current time
+                        var expiration = jwtToken?.ValidTo;
+                        if (expiration.HasValue && expiration.Value < DateTime.UtcNow)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            await context.Response.WriteAsync("Token has expired");
+                            return;
+                        }
+
+                        // Proceed if token is valid and not expired
+                    }
+                    catch (SecurityTokenException)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        await context.Response.WriteAsync("Invalid token or secret key mismatch");
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        await context.Response.WriteAsync("Token validation failed");
+                        return;
+                    }
+                }
+            }
+
+            await next(context);
+        }
+    }
+}
