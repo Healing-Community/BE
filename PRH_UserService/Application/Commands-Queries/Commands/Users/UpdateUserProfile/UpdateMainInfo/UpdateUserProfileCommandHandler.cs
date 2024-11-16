1ce4fe32755ff190fd36deaf8ac2b7d779a5bfa8
nghia46 <nghialoe46@gmail.com>
2024-11-17 00:47:14 +0700

    fix user profile

--- PRH_UserService/Application/Commands-Queries/Commands/Users/UpdateUserProfile/UpdateMainInfo/UpdateUserProfileCommandHandler.cs
+++ PRH_UserService/Application/Commands-Queries/Commands/Users/UpdateUserProfile/UpdateMainInfo/UpdateUserProfileCommandHandler.cs
@@ -1,14 +1,14 @@
-using System;
 using System.Security.Claims;
 using Application.Commons;
 using Application.Interfaces.Repository;
 using MassTransit;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-
+using Domain.Entities;
+using NUlid;
 namespace Application.Commands_Queries.Commands.Users.UpdateUserProfile;
 
-public class UpdateUserProfileCommandHandler(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor) : IRequestHandler<UpdateUserProfileCommand, DetailBaseResponse<string>>
+public class UpdateUserProfileCommandHandler(ISocialLinkRepository socialLinkRepository, IUserRepository userRepository, IHttpContextAccessor httpContextAccessor) : IRequestHandler<UpdateUserProfileCommand, DetailBaseResponse<string>>
 {
     public async Task<DetailBaseResponse<string>> Handle(UpdateUserProfileCommand request, CancellationToken cancellationToken)
     {
@@ -18,31 +18,41 @@ public class UpdateUserProfileCommandHandler(IUserRepository userRepository, IHt
 
             if (userId == null)
             {
-                return new DetailBaseResponse<string>
-                {
-                    Id = NewId.NextGuid().ToString(),
-                    Message = "Không tìm thấy thông tin người dùng từ token",
-                    Success = false
-                };
+                return DetailBaseResponse<string>.Unauthorized();
             }
-
 
             var user = await userRepository.GetByIdAsync(userId);
             if (user == null)
             {
-                return new DetailBaseResponse<string>
-                {
-                    Id = NewId.NextGuid().ToString(),
-                    Message = "Không tìm thấy thông tin người dùng",
-                    Success = false
-                };
+                return DetailBaseResponse<string>.NotFound();
             }
-
+            // Update user information
+            user.ProfilePicture = request.UserDto.ProfilePictureUrl;
             user.FullName = request.UserDto.FullName;
             user.PhoneNumber = request.UserDto.PhoneNumber;
             user.Descrtiption = request.UserDto.Descrtiption;
-
             await userRepository.UpdateAsync(userId, user);
+            // Update social link
+            foreach (var item in request.UserDto.SocialLinks)
+            {
+                var link = await socialLinkRepository.GetByPropertyAsync(u => u.UserId == userId && u.PlatformName == item.PlatformName);
+                if (item.PlatformName == link?.PlatformName)
+                {
+                    link.Url = item.Url;
+                    await socialLinkRepository.UpdateAsync(link.LinkId, link);
+                }
+                else
+                {
+                    var socialLink = new SocialLink
+                    {
+                        LinkId = Ulid.NewUlid().ToString(),
+                        PlatformName = item.PlatformName,
+                        Url = item.Url,
+                        UserId = userId
+                    };
+                    await socialLinkRepository.Create(socialLink);
+                }
+            }
         }
         catch
         {
--- PRH_UserService/Application/Commons/DetailBaseResponse.cs
+++ PRH_UserService/Application/Commons/DetailBaseResponse.cs
@@ -1,4 +1,4 @@
-﻿using MassTransit;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 
 namespace Application.Commons;
@@ -13,7 +13,7 @@ public class DetailBaseResponse<T>
     public List<ErrorDetail>? Errors { get; set; }
     public DateTime Timestamp { get; set; }
 
-    internal static DetailBaseResponse<T> SuccessReturn(string Message,T classInstance)
+    internal static DetailBaseResponse<T> SuccessReturn(string Message, T classInstance)
     {
         return new DetailBaseResponse<T>
         {
@@ -25,4 +25,38 @@ public class DetailBaseResponse<T>
             Timestamp = DateTime.UtcNow
         };
     }
+    internal static DetailBaseResponse<T> InternalServerError(string message)
+    {
+        return new DetailBaseResponse<T>
+        {
+            Id = NewId.NextSequentialGuid().ToString(),
+            StatusCode = StatusCodes.Status500InternalServerError,
+            Message = message,
+            Success = false,
+            Timestamp = DateTime.UtcNow
+        };
+    }
+    internal static DetailBaseResponse<string> Unauthorized()
+    {
+        return new DetailBaseResponse<string>
+        {
+            Id = NewId.NextSequentialGuid().ToString(),
+            StatusCode = StatusCodes.Status401Unauthorized,
+            Message = "Không có quyền truy cập, chưa đăng nhập hoặc phiên làm việc hết hạn",
+            Success = false,
+            Timestamp = DateTime.UtcNow
+        };
+    }
+    internal static DetailBaseResponse<T> NotFound(string message = "Không tìm thấy dữ liệu")
+    {
+        return new DetailBaseResponse<T>
+        {
+            Id = NewId.NextSequentialGuid().ToString(),
+            StatusCode = StatusCodes.Status404NotFound,
+            Message = message,
+            Success = false,
+            Timestamp = DateTime.UtcNow
+        };
+    }
+
 }
