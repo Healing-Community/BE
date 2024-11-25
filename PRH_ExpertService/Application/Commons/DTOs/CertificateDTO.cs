9eca7dd9c1cab73cd186224878abcf054f0f6f06
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-11-25 21:28:25 +0700

    add field

--- PRH_ExpertService/Application/Commons/DTOs/CertificateDTO.cs
+++ PRH_ExpertService/Application/Commons/DTOs/CertificateDTO.cs
@@ -7,5 +7,6 @@
         public DateOnly IssueDate { get; set; }
         public DateOnly ExpirationDate { get; set; }
         public int Status { get; set; }
+        public string FileUrl { get; set; }
     }
 }
