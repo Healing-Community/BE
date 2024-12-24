73da2c9390fecab21a8f69160af23f6b2c2567ff
nghia46 <nghialoe46@gmail.com>
2024-12-24 16:16:59 +0700

    add edit and delete share

---
+++ PRH_PostService/Application/Commons/DTOs/EditShareDto.cs
@@ -0,0 +1,7 @@
+namespace Application.Commons.DTOs;
+
+public class EditShareDto
+{
+    public required string ShareId { get; set; }
+    public string? Description { get; set; }
+}
--- PRH_PostService/PRH_PostService_API/Controllers/ShareController.cs
+++ PRH_PostService/PRH_PostService_API/Controllers/ShareController.cs
@@ -1,4 +1,6 @@
 using Application.Commads_Queries.Commands.CreateShare;
+using Application.Commads_Queries.Commands.Share.DeleteShare;
+using Application.Commads_Queries.Commands.Share.UpdateShare;
 using Application.Commads_Queries.Queries.Share;
 using Application.Commons.DTOs;
 using MediatR;
@@ -34,6 +36,30 @@ namespace PRH_PostService_API.Controllers
         public async Task<IActionResult> ShareStory([FromBody] ShareDto shareDto)
         {
             var response = await sender.Send(new CreateShareCommand(shareDto));
+            return response.ToActionResult();
+        }
+        /// <summary>
+        /// Cập nhật bài viết đã chia sẻ
+        /// </summary>
+        /// <param name="editShareDto"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPut("update-share")]
+        public async Task<IActionResult> UpdateShare([FromBody] EditShareDto editShareDto)
+        {
+            var response = await sender.Send(new UpdateShareCommand(editShareDto));
+            return response.ToActionResult();
+        }
+        /// <summary>
+        /// Xóa bài viết đã chia sẻ
+        /// </summary>
+        /// <param name="shareId"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpDelete("delete-share/{shareId}")]
+        public async Task<IActionResult> DeleteShare(string shareId)
+        {
+            var response = await sender.Send(new DeleteShareCommand(shareId));
             return response.ToActionResult();
         }
     }
