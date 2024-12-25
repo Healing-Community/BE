fa63f2720adf0d798fb7d583b23e0edb0c51d084
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-25 10:51:32 +0700

    get all comment by share id

---
+++ PRH_PostService/Application/Commads-Queries/Queries/Comments/GetCommentsByShareId/GetCommentsByShareIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons.DTOs;
+using Application.Commons;
+using MediatR;
+
+namespace Application.Commads_Queries.Queries.Comments.GetCommentsByShareId
+{
+    public record GetCommentsByShareIdQuery(string ShareId) : IRequest<BaseResponse<List<CommentDtoResponse>>>;
+}
