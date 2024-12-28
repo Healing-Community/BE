32e370142142dc858d924db618c868f6141009b4
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-28 16:15:03 +0700

    feat: finish API get post in group by user and group id

---
+++ PRH_PostService/Application/Commads-Queries/Queries/Posts/GetPostsInGroupByUserAndGroup/GetPostsInGroupByUserAndGroupQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Commons.DTOs;
+using Application.Commons;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Commads_Queries.Queries.Posts.GetPostsInGroupByUserAndGroup
+{
+    public record GetPostsInGroupByUserAndGroupQuery(string GroupId, string UserId, HttpContext HttpContext)
+        : IRequest<BaseResponse<List<PostGroupWithoutGroupIDAndUserID>>>;
+}
