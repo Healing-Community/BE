635bb001558b3c866bc7be447ff89c524084f468
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-28 20:41:20 +0700

    feat: finish API count total comment by share id

---
+++ PRH_PostService/Application/Commads-Queries/Queries/Comments/CountTotalCommentByShareId/CountTotalCommentByShareIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Commons;
+using MediatR;
+
+namespace Application.Commads_Queries.Queries.Comments.CountTotalCommentByShareId
+{
+    public record CountTotalCommentByShareIdQuery(string ShareId) : IRequest<BaseResponse<object>>;
+}
