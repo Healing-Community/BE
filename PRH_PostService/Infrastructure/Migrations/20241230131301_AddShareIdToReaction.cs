46af913fe3cc4c2591b7b1c37a7eba29e333e342
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-30 20:54:16 +0700

    modified: migrations add share id to reaction

---
+++ PRH_PostService/Infrastructure/Migrations/20241230131301_AddShareIdToReaction.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShareIdToReaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShareId",
+                table: "Reactions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reactions_ShareId",
+                table: "Reactions",
+                column: "ShareId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reactions_Shares_ShareId",
+                table: "Reactions",
+                column: "ShareId",
+                principalTable: "Shares",
+                principalColumn: "ShareId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reactions_Shares_ShareId",
+                table: "Reactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reactions_ShareId",
+                table: "Reactions");
+
+            migrationBuilder.DropColumn(
+                name: "ShareId",
+                table: "Reactions");
+        }
+    }
+}
