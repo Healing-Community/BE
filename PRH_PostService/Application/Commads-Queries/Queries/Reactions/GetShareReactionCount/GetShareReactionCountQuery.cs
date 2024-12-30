ca444d95bd1f5bb773cd5fe8951a309cdabb5404
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-30 20:57:57 +0700

    feat: create logic for Get Share Reaction Count by Share ID

---
+++ PRH_PostService/Application/Commads-Queries/Queries/Reactions/GetShareReactionCount/GetShareReactionCountQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons.DTOs;
+using Application.Commons;
+using MediatR;
+
+namespace Application.Commads_Queries.Queries.Reactions.GetShareReactionCount
+{
+    public record GetShareReactionCountQuery(ShareIdOnlyDto ShareId) : IRequest<BaseResponse<PostReactionCountDto>>;
+}
