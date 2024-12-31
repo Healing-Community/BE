201fad6028ce3332096660208c95099c9215c31c
dattgt <trangiathanhdat02092002@gmail.com>
2025-01-01 00:28:31 +0700

    feat: migrations for adding group creation request table

--- PRH_GroupService/Infrastructure/Migrations/20241231151639_FixLogicDB.Designer.cs
+++ PRH_GroupService/Infrastructure/Migrations/20241231151639_FixLogicDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(HFDBGroupServiceContext))]
-    partial class HFDBGroupServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20241231151639_FixLogicDB")]
+    partial class FixLogicDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -119,7 +122,7 @@ namespace Infrastructure.Migrations
                     b.Property<bool?>("IsApproved")
                         .HasColumnType("boolean");
 
-                    b.Property<DateTime?>("RequestedAt")
+                    b.Property<DateTime>("RequestedAt")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("timestamp with time zone")
                         .HasDefaultValueSql("NOW()");
