b57eee33cd88b3c1bb7a4213ce9b47d4eb155ed8
dattgt <trangiathanhdat02092002@gmail.com>
2025-01-01 00:32:29 +0700

    feat: create group request for user

---
+++ PRH_GroupService/Application/Commands/GroupRequests/CreateGroupRequest/CreateGroupRequestCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Commons;
+using MediatR;
+
+namespace Application.Commands.GroupRequests.CreateGroupRequest
+{
+    public record CreateGroupRequestCommand(string GroupName, string Description) : IRequest<BaseResponse<string>>;
+}
