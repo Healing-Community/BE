70970f24666db291df6b60269d442d8739716578
dattgt <trangiathanhdat02092002@gmail.com>
2024-12-31 21:21:26 +0700

    feat: create API get user reaction by share id

---
+++ PRH_PostService/Application/Commads-Queries/Queries/Reactions/GetUserReactionByShareId/GetUserReactionByShareIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commads_Queries.Queries.Reactions.GetUserReactionByShareId
+{
+    public record GetUserReactionByShareIdQuery(string ShareId) : IRequest<BaseResponse<Reaction>>;
+}
