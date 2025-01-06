e7f9b8da049bd110da3be0c27a42d7f8371f467b
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2025-01-06 21:10:36 +0700

    feat: expert dashboard

---
+++ PRH_ExpertService/Application/Commands-Quereis/Queries/GetRecentRatings/GetRecentRatingsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Application.Commons.DTOs;
+using Application.Commons;
+
+namespace Application.Queries.GetRecentRatings
+{
+    public class GetRecentRatingsQuery : IRequest<BaseResponse<IEnumerable<RecentRatingDTO>>>
+    {
+    }
+}
---
+++ PRH_ExpertService/Application/Commons/DTOs/RecentRatingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Commons.DTOs
+{
+    public class RecentRatingDTO
+    {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+    }
+}
