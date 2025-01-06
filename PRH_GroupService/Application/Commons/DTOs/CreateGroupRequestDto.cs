127dde0e8d1e660296f22b1d80ae43e0380b9702
dattgt <trangiathanhdat02092002@gmail.com>
2025-01-06 12:36:04 +0700

    modifed API create request group

--- PRH_GroupService/Application/Commons/DTOs/CreateGroupRequestDto.cs
+++ PRH_GroupService/Application/Commons/DTOs/CreateGroupRequestDto.cs
@@ -4,5 +4,6 @@
     {
         public required string GroupName { get; set; }
         public string Description { get; set; } = string.Empty;
+        public string CoverImg { get; set; } = string.Empty;
     }
 }
