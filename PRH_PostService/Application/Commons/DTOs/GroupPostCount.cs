9f4df11b92987e39a58347be47ed2d65d8e599c6
dattgt <trangiathanhdat02092002@gmail.com>
2025-01-08 12:28:26 +0700

    create dto

---
+++ PRH_PostService/Application/Commons/DTOs/GroupPostCount.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Commons.DTOs
+{
+    public class GroupPostCount
+    {
+        public string GroupId { get; set; } = string.Empty;
+        public int PostCount { get; set; }
+    }
+}
