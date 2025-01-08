3c667a61bb22146986d05ca89105773c0d811160
nghia46 <nghialoe46@gmail.com>
2025-01-08 13:13:01 +0000

    cancel appointment grpc update

--- PRH_PaymentService/Infrastructure/Mapper/MappingProfile.cs
+++ PRH_PaymentService/Infrastructure/Mapper/MappingProfile.cs
@@ -7,20 +7,8 @@ namespace Infrastructure.Mapper
     {
         public MappingProfile()
         {
-            CreateMap<string, PaymentStatus>()
-                .ConvertUsing(status => MapPayOSStatusToPaymentStatus(status));
+           
         }
 
-        private static PaymentStatus MapPayOSStatusToPaymentStatus(string payOSStatus)
-        {
-            return payOSStatus switch
-            {
-                "PENDING" => PaymentStatus.Pending,
-                "PAID" => PaymentStatus.Paid,
-                "FAILED" => PaymentStatus.Failed,
-                "CANCELLED" => PaymentStatus.Cancelled,
-                _ => PaymentStatus.Unknown
-            };
-        }
     }
 }
