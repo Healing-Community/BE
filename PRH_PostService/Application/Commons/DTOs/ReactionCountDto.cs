0e421f4cbe4875146909ed6521a90f1ee131b418
dattgt <trangiathanhdat02092002@gmail.com>
2025-01-13 22:09:07 +0700

    feat: create API count post and API count reaction

---
+++ PRH_PostService/Application/Commons/DTOs/ReactionCountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Commons.DTOs
+{
+    public class ReactionCountDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int ReactionCount { get; set; }
+    }
+}
