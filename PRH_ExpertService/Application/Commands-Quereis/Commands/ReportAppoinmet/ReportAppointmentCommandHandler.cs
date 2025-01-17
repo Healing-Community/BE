1b583e2c0dcfa90b727450c86a768b7f8c19d053
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2025-01-17 15:25:57 +0700

    fix mail form for report appointment

--- PRH_ExpertService/Application/Commands-Quereis/Commands/ReportAppoinmet/ReportAppointmentCommandHandler.cs
+++ PRH_ExpertService/Application/Commands-Quereis/Commands/ReportAppoinmet/ReportAppointmentCommandHandler.cs
@@ -65,14 +65,43 @@ public class ReportAppointmentCommandHandler(IGrpcHelper grpcHelper,IMessagePubl
             {
                 To = userReply.Email,
                 Subject = "Báo cáo lịch hẹn",
-                Body = $"Lịch hẹn với chuyên giai '{expertReply.UserName}' của bạn đã được báo cáo thành công và sẽ được xử lý trong thời gian sớm nhất"
+                Body = $@"
+                <html>
+                <body style=""margin: 0; padding: 0; font-family: 'Verdana', sans-serif; background-color: #f0f4f8;"">
+                    <div style=""max-width: 650px; margin: 0 auto; background-color: #fff; padding: 30px; border-radius: 10px; box-shadow: 0 6px 20px rgba(0, 0, 0, 0.1);"">
+                        <div style=""text-align: center;"">
+                            <img src=""https://i.postimg.cc/zXN0D5kY/logo.png"" alt=""Healing Image"" style=""max-width: 100%; height: auto; border-radius: 8px;"">
+                        </div>
+                        <h2 style=""color: #4caf50; text-align: center; margin-top: 20px;"">Báo cáo</h2>
+                        <p style=""font-size: 17px; line-height: 1.8; color: #444; text-align: justify;"">Báo cáo của bạn đã được ghi nhận và sẽ được xử lí trong thời gian sớm nhất!</p>
+                        <p style=""text-align: center; color: #999; font-size: 13px;"">&copy; 2024 Healing Community. Tất cả các quyền được bảo lưu.</p>
+                    </div>
+                </body>
+                </html>
+                ";
             };
             var expertMail = new SendMailMessage
             {
                 To = expertReply.Email,
                 Subject = "Báo cáo lịch hẹn",
-                Body = $"Lịch hẹn từ '{appointment.StartTime}' đến '{appointment.EndTime}' vào ngày '{appointment.AppointmentDate}' của bạn đã bị báo cáo bởi {userReply.UserName}. Vui lòng cung cấp thông tin video hoặc hình ảnh để chứng minh sự việc. \n Hãy trả lời tin nhắn này và gửi đính kèm đường link hoặc file hình ảnh, video để được duyệt. \n Nội dung báo cáo: {request.ReportDescription} \n Xin cảm ơn"
+                Body = $@"
+                <html>
+                <body style=""margin: 0; padding: 0; font-family: 'Verdana', sans-serif; background-color: #f0f4f8;"">
+                    <div style=""max-width: 650px; margin: 0 auto; background-color: #fff; padding: 30px; border-radius: 10px; box-shadow: 0 6px 20px rgba(0, 0, 0, 0.1);"">
+                        <div style=""text-align: center;"">
+                            <img src=""https://i.postimg.cc/zXN0D5kY/logo.png"" alt=""Healing Image"" style=""max-width: 100%; height: auto; border-radius: 8px;"">
+                        </div>
+                        <h2 style=""color: #4caf50; text-align: center; margin-top: 20px;"">Báo cáo</h2>
+                        <p style=""font-size: 17px; line-height: 1.8; color: #444; text-align: justify;"">Lịch hẹn từ '{appointment.StartTime}' đến '{appointment.EndTime}' vào ngày '{appointment.AppointmentDate}' của bạn đã bị báo cáo bởi {userReply.UserName}. Vui lòng cung cấp thông tin video hoặc hình ảnh để chứng minh sự việc. \n Hãy trả lời tin nhắn này và gửi đính kèm đường link hoặc file hình ảnh, video để được duyệt. \n Nội dung báo cáo: {request.ReportDescription}</p>
+                        <p style=""text-align: center; color: #999; font-size: 13px;"">&copy; 2024 Healing Community. Tất cả các quyền được bảo lưu.</p>
+                    </div>
+                </body>
+                </html>
+                ";
             };
+
+            await messagePublisher.PublishAsync(userMail, QueueName.MailQueue, cancellationToken);
+            await messagePublisher.PublishAsync(expertMail, QueueName.MailQueue, cancellationToken);
 
             return BaseResponse<string>.SuccessReturn("Báo cáo của bạn đã được ghi nhận và sẽ được xử lý trong thời gian sớm nhất");  
 
