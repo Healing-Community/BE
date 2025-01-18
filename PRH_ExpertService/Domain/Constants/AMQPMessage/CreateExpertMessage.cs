517e11df300f8d965b804787ff82442bd9b5de26
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2025-01-18 23:12:58 +0700

    fix

---
+++ PRH_ExpertService/Domain/Constants/AMQPMessage/CreateExpertMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Constants.AMQPMessage;
+
+public class CreateExpertMessage
+{
+    public required string UserId { get; set; }
+    public required string UserName { get; set; }
+}
---
+++ PRH_ExpertService/PRH_ExpertService_API/Consumer/CreateExpertProfileServiceConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces.Repository;
+using Domain.Constants.AMQPMessage;
+using MassTransit;
+
+namespace PRH_ExpertService_API.Consumer
+{
+    public class CreateExpertProfileServiceConsumer (IExpertProfileRepository expertProfileRepository) : IConsumer<CreateExpertMessage>
+    {
+        public async Task Consume(ConsumeContext<CreateExpertMessage> context)
+        {
+            var message = context.Message;
+
+            await expertProfileRepository.Create(new Domain.Entities.ExpertProfile
+            {
+                ExpertProfileId = message.UserId,
+                UserId = message.UserId,
+                Fullname = string.Empty,
+                Email = string.Empty,
+                Specialization = string.Empty,
+                ExpertiseAreas = string.Empty,
+                CreatedAt = DateTime.UtcNow.AddHours(7),
+                UpdatedAt = DateTime.UtcNow.AddHours(7)
+            });
+        }
+    }
+}
