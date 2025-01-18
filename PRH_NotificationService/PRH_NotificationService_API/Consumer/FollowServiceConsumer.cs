8f62982f80210763c491834dbd4184289cdc7b5c
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2025-01-19 00:16:28 +0700

    fix

--- PRH_NotificationService/PRH_NotificationService_API/Consumer/FollowServiceConsumer.cs
+++ PRH_NotificationService/PRH_NotificationService_API/Consumer/FollowServiceConsumer.cs
@@ -12,19 +12,19 @@ namespace PRH_NotificationService_API.Consumer
         {
             var followRequest = context.Message;
 
-            var notificationType = await notificationTypeRepository.GetByIdAsync("02") ?? throw new Exception("Notification type not found");
+            var notificationType = await notificationTypeRepository.GetByIdAsync("02");
             var notification = new Notification
             {
                 NotificationId = Ulid.NewUlid().ToString(),
                 UserId = followRequest.FollowerId,
                 NotificationTypeId = notificationType.NotificationTypeId,
-                Message = $"Người dùng {followRequest.FollowerId} đã theo dõi người dùng {followRequest.FollowedUserId}",
+                Message = $"Người dùng {followRequest.FollowingUserName} đã theo dõi người dùng {followRequest.FollowedUserName}",
                 CreatedAt = followRequest.FollowDate,
                 UpdatedAt = followRequest.FollowDate,
                 IsRead = false
             };
 
-            await notificationRepository.CreateNotificationsAsync([notification]);
+            await notificationRepository.CreateNotificationsAsync(new List<Notification> { notification });
         }
     }
 }
